b7f9026dd1002db0d59627202d89094b0747dd27
Anesu <anesu4@yahoo.com.au>
2021-04-20 13:34:47 +1000

    Initial - Create SS, SL and CL script files with relevant ConSolidation Invoice details

--- mp_cl_consol_inv.js
+++ mp_cl_consol_inv.js
@@ -15,6 +15,24 @@
  * 
  */
 
+
+ /**************************
+ *  
+ *  Workflow: Code Edition
+ *  
+ **************************
+ * 
+ * User Input:
+ * 
+ * 1. Accounts Selects Zee to View. Customer dropdown appears
+ * 2. Accounts Selects Parent Consolidation Customer - Search = 2722
+ * 3. User Selects Invoice Type (Branch, Invoice Type, State, Multi-Parent)
+ * 4. User clicks Generate Invoice
+ *  
+ * 
+ *  Script Output:
+ *  1. Generate Invoice
+ */
 define(['N/error', 'N/runtime', 'N/search', 'N/url', 'N/record', 'N/format', 'N/email', 'N/currentRecord'],
   function(error, runtime, search, url, record, format, email, currentRecord) {
     var baseURL = 'https://1048144.app.netsuite.com';
@@ -27,7 +45,104 @@ define(['N/error', 'N/runtime', 'N/search', 'N/url', 'N/record', 'N/format', 'N/
      * On page initialisation
      */
     function pageInit() {
+        var zee = 0;
+        var custid = 0;
+
+        /**
+             *  Click for Instructions Section Collapse
+             */
+         $('.collapse').on('shown.bs.collapse', function() {
+            $(".range_filter_section_top").css("padding-top","500px");
+        });
+        $('.collapse').on('hide.bs.collapse', function() {
+            $(".range_filter_section_top").css("padding-top","0px");
+        });
+
+        $('.zee_dropdown').on('change', function(){
+            zee = $(this).val();
+            var upload_url = baseURL + url.resolveScript({
+                deploymentId: 'customdeploy_cl_consol_inv',
+                scriptId: 'customscript_cl_consol_inv'
+            }) + '&zee=' + zee; 
+            window.open(upload_url, "_self", "height=750,width=650,modal=yes,alwaysRaised=yes")
+        });
+        $('.cus_dropdown').on('change', function(){
+            custid = $(this).val();
+            var upload_url = baseURL + url.resolveScript({
+                deploymentId: 'customdeploy_cl_consol_inv',
+                scriptId: 'customscript_cl_consol_inv'
+            }) + '&custid=' + custid;
+        });
+        $('.inv_type_dropdown').on('change', function(){
+            inv_type = $(this).val();
+            var upload_url = baseURL + url.resolveScript({
+                deploymentId: 'customdeploy_cl_consol_inv',
+                scriptId: 'customscript_cl_consol_inv'
+            }) + '&inv_type=' + inv_type;
+        });
+        if(!isNullorEmpty($('.inv_type').val())){
+            $('.export_csv').removeClass('hide')
+        }
+    }
+
+    function generateInvoice(){
+
+    }
+
+    function reloadPageWithParams(){
+        var params = [];
+        var upload_url = baseURL + url.resolveScript({
+            deploymentId: 'customdeploy_cl_consol_inv',
+            scriptId: 'customscript_cl_consol_inv'
+        }); 
+        window.open(upload_url, "_self", "height=750,width=650,modal=yes,alwaysRaised=yes")
+    }
+    
+    function onButtonClick(scriptContext) {
+        dialog.alert({
+            title: "Alert",
+            message: "You click this export button!"
+        });
+       
+        // XML content of the file
+        var xmlStr = '<?xml version="1.0"?><?mso-application progid="Excel.Sheet"?>';
+        xmlStr += '<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" ';
+        xmlStr += 'xmlns:o="urn:schemas-microsoft-com:office:office" ';
+        xmlStr += 'xmlns:x="urn:schemas-microsoft-com:office:excel" ';
+        xmlStr += 'xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" '; 
+        xmlStr += 'xmlns:html="http://www.w3.org/TR/REC-html40">';
+        xmlStr += '<Worksheet ss:Name="Sheet1">';
+        xmlStr += '<Table>' +
+        '<Row>' +
+        '<Cell><Data ss:Type="String"> First Header </Data></Cell>' +
+        '<Cell><Data ss:Type="String"> Second Header </Data></Cell>' +
+        '<Cell><Data ss:Type="String"> Third Header </Data></Cell>' +
+        '<Cell><Data ss:Type="String"> Fourth Header </Data></Cell>' +
+        '<Cell><Data ss:Type="String"> Fifth Header </Data></Cell>' +
+        '</Row>';
+        xmlStr += '</Table></Worksheet></Workbook>';
         
+        // Encode Contents
+        var base64EncodedString = encode.convert({
+            string: xmlStr,
+            inputEncoding: encode.Encoding.UTF_8,
+            outputEncoding: encode.Encoding.BASE_64
+        });
+        
+        // Create File 
+        var xlsFile = file.create({ 
+            name: 'TEST.xls',
+            fileType: 'EXCEL',
+            contents: base64EncodedString
+        });
+
+        log.debug({
+            details: "File ID: " + fileid
+        });
+
+        scriptContext.response.writeFile({
+            file : xlsFile
+        });
     }
 
     function saveRecord(context) {
--- mp_sl_consol_inv.js
+++ mp_sl_consol_inv.js
@@ -15,35 +15,268 @@
  * 
  */
 
- define(['N/ui/serverWidget', 'N/email', 'N/runtime', 'N/search', 'N/record', 'N/http', 'N/log', 'N/redirect', 'N/format'], 
- function(ui, email, runtime, search, record, http, log, redirect, format) {
-    var baseURL = 'https://1048144.app.netsuite.com';
-    if (runtime.EnvType == "SANDBOX") {
-        baseURL = 'https://1048144-sb3.app.netsuite.com';
-    }
-    var zee = 0;
-    var role = runtime.getCurrentUser().role;
-    if (role == 1000) {
-        //Franchisee
-        zee = runtime.getCurrentUser();
-    } 
-
-    function onRequest(context) {  
-        if (context.request.method === 'GET') {
-            var form = ui.createForm({ title: 'Consolidation Invoice'})
-
-
-        } else {
-
+define(['N/ui/serverWidget', 'N/email', 'N/runtime', 'N/search', 'N/record', 'N/http', 'N/log', 'N/redirect', 'N/format'],
+    function (ui, email, runtime, search, record, http, log, redirect, format) {
+        var baseURL = 'https://1048144.app.netsuite.com';
+        if (runtime.EnvType == "SANDBOX") {
+            baseURL = 'https://1048144-sb3.app.netsuite.com';
         }
-    }
+        var zee = 0;
+        var role = runtime.getCurrentUser().role;
+        if (role == 1000) {
+            //Franchisee
+            zee = runtime.getCurrentUser();
+        }
 
-    function isNullorEmpty(strVal) {
-        return (strVal == null || strVal == '' || strVal == 'null' || strVal == undefined || strVal == 'undefined' || strVal == '- None -');
-    }
-    
-    return {
-        onRequest: onRequest
-    };
- 
- });
+        function onRequest(context) {
+            if (context.request.method === 'GET') {
+                var is_params = false;
+
+                var params = context.request.parameters;
+                var zee_id = 0;
+                var custid = 0;
+                var inv_type;
+                var period;
+
+                if (!isNullorEmpty(params)) {
+                    is_params == true;
+
+                    zee_id = params.zee;
+                    custid = params.custid;
+                    inv_type = params.inv;
+                    period = params.period;
+                }
+
+                var form = ui.createForm({ title: 'Consolidation Invoice' });
+
+                // Load jQuery
+                var inlineHtml = '<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>';
+                // Load Tooltip
+                inlineHtml += '<script src="https://unpkg.com/@popperjs/core@2"></script>';
+
+                // Load Bootstrap
+                inlineHtml += '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">';
+                inlineHtml += '<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>';
+                // Load DataTables
+                inlineHtml += '<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">';
+                inlineHtml += '<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>';
+
+                // Load Bootstrap-Select
+                inlineHtml += '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">';
+                inlineHtml += '<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>';
+
+                // Load Netsuite stylesheet and script
+                inlineHtml += '<link rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2060796&c=1048144&h=9ee6accfd476c9cae718&_xt=.css"/>';
+                inlineHtml += '<script src="https://1048144.app.netsuite.com/core/media/media.nl?id=2060797&c=1048144&h=ef2cda20731d146b5e98&_xt=.js"></script>';
+                inlineHtml += '<link type="text/css" rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2090583&c=1048144&h=a0ef6ac4e28f91203dfe&_xt=.css">';
+                inlineHtml += '<style>.mandatory{color:red;}</style>';
+
+                // Popup Notes Section
+                inlineHtml += '<div id="myModal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true"><div class="modal-dialog modal-sm" role="document" style="width :max-content"><div class="modal-content" style="width :max-content; max-width: 900px"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title panel panel-info" id="exampleModalLabel">Notes Section</h4><br> </div><div class="modal-body"></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';
+                inlineHtml += '<div id="myModal2" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true"><div class="modal-dialog modal-sm" role="document" style="width :max-content"><div class="modal-content" style="width :max-content; max-width: 900px"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title panel panel-info" id="exampleModalLabel">Snooze Timers</h4><br> </div><div class="modal-body"></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';
+
+                // Click for Instructions
+                inlineHtml += '<button type="button" class="btn btn-sm btn-info instruction_button" data-toggle="collapse" data-target="#demo">Click for Instructions</button><div id="demo" style="background-color: #cfeefc !important;border: 1px solid #417ed9;padding: 10px 10px 10px 20px;width:96%;position:absolute" class="collapse"><b><u>IMPORTANT INSTRUCTIONS:</u></b>';
+                inlineHtml += '<ul><li><input type="button" class="btn-xs" style="background-color: #fff; color: black;" disabled value="Submit Search" /> - <ul><li>Click "Submit Search" to load Datatable using current parameters</li></ul></li>'
+                inlineHtml += '<li>Functionalities available on the Debt Collections Table:<ul><li><b>Sort</b><ul><li>Click on column headers to sort collections invoices according to the values in the columns. This is default to "Days Overdue".</li><li>Hold "Shift" and click another column to sort according to multiple columns.</li></ul></li><li><b>Search</b><ul><li>You can search for specific Customer or Invoice by typing into the "Search" field</li></ul></li></ul></li>';
+                inlineHtml += '<li>Table Filters:<ul><li><b>Matching MAAP Allocation</b><ul><li><button type="button" class="btn-xs btn-success " disabled><span class="glyphicon glyphicon-plus"></span></button> - Click to apply MAAP Allocation filters search filters on table. ONLY click once. </li><li><button type="button" class="btn-xs btn-danger " disabled><span class="glyphicon glyphicon-minus"></span></button> - Click to remove MAAP Allocation search filter from table. This is set default to "Days Overdue".</li></ul></li> <li><b>MP Ticket Column</b><ul><button type="button" class="btn-xs btn-success" disabled><span class="glyphicon glyphicon-plus"></span></button> - Click to apply MAAP Allocation filters search filters on table. ONLY click once. </li></ul></li></ul></li><li>Clickable Actions Available Per Invoice in DataTable:</li>';
+                inlineHtml += '<ul><li><button type="button" class="btn-xs" disabled><span class="span_class glyphicon glyphicon-pencil"></span></button> - Click to open Notes Section for Selected Invoice. (Notes Section is seperate from User Notes)</li>';
+                inlineHtml += '<li><button type="button" class="btn-xs btn-secondary" disabled><span class="glyphicon glyphicon-eye-open"></span></button> - Click to Set Invoice has "Viewed" by a member of the Finance Team.</li>';
+                inlineHtml += '<li><button type="button" class="btn-xs btn-info" disabled><span class="glyphicon glyphicon-time"></span></button> - Click to view Snooze Timers</li><li><button type="button" class="timer-1day form=control btn-xs btn-info" disabled><span class="span_class">1 Day</span></button> - Click to select Snooze duration of invoice from Debt Collections Page.</li>';
+                inlineHtml += '</li></ul></div>';
+
+                inlineHtml += invTypeDropdownSection();
+                inlineHtml += zeeDropdownSection();
+                inlineHtml += parentDropdownSection();
+                inlineHtml += custDropdownSection();
+                inlineHtml += subCustDropdownSection();
+                inlineHtml += generateInvoice();
+
+                form.addField({
+                    id: 'preview_table',
+                    label: 'inlinehtml',
+                    type: 'inlinehtml'
+                }).updateLayoutType({
+                    layoutType: ui.FieldLayoutType.STARTROW
+                }).defaultValue = inlineHtml;
+
+                var zee_id = 0;
+                var custid = 0;
+                var inv_type;
+                var period;
+
+                form.addField({
+                    id: 'custpage_consol_inv_zee',
+                    label: 'Zee ID',
+                    type: ui.FieldType.TEXT
+                }).updateDisplayType({
+                    displayType: ui.FieldDisplayType.HIDDEN
+                }).defaultValue = zee_id;
+                form.addField({
+                    id: 'custpage_consol_inv_custid',
+                    label: 'Customer ID',
+                    type: ui.FieldType.TEXT
+                }).updateDisplayType({
+                    displayType: ui.FieldDisplayType.HIDDEN
+                }).defaultValue = custid;
+                form.addField({
+                    id: 'custpage_consol_inv_inv_type',
+                    label: 'Invoice Type',
+                    type: ui.FieldType.TEXT
+                }).updateDisplayType({
+                    displayType: ui.FieldDisplayType.HIDDEN
+                }).defaultValue = inv_type;
+                form.addField({
+                    id: 'custpage_consol_inv_period',
+                    label: 'Period',
+                    type: ui.FieldType.TEXT
+                }).updateDisplayType({
+                    displayType: ui.FieldDisplayType.HIDDEN
+                }).defaultValue = period;
+
+                form.clientScriptFileId = 4750772; // 4607145
+
+                context.response.writePage(form);
+
+            } else {
+
+            }
+        }
+
+        function invTypeDropdownSection() {
+            var inlineQty = '<div class="col-lg-12 invTypeDropdownSection">';
+            inlineQty += '<div class="col-xs-6">';
+            inlineQty += '<div class="input-group"><span class="input-group-addon" id="showMAAP_box">Invoice Type</span></div>';
+            inlineQty += '<select id="zee_dropdown" class="form-control">';
+            inlineQty += '<option value="1">Branch</option>';
+            inlineQty += '<option value="2">Invoice Type</option>';
+            inlineQty += '<option value="3">State</option>';
+            inlineQty += '<option value="4">Multi-Parent</option>';
+            inlineQty += '</select>';
+            inlineQty += '</div></div>';
+
+            return inlineQty;
+        }
+
+        function zeeDropdownSection() {
+            var inlineQty = '<div class="col-lg-12 zeeDropdown">';
+            inlineQty += '<div class="col-xs-6">';
+            inlineQty += '<div class="input-group"><span class="input-group-addon" id="showMAAP_box">Franchisee</span></div>';
+            inlineQty += '<select id="zee_dropdown" class="form-control" required>';
+            inlineQty += '<option></option>';
+            var zeesSearch = search.load({ type: 'partner', id: 'customsearch_job_inv_process_zee' });
+            var zeesSearchResults = zeesSearch.run();
+            zeesSearchResults.each(function(zeesSearchResult) {
+                var zee_id = zeesSearchResult.getValue({ name: 'internalid', summmary: search.Operator.GROUP });
+                var zee_name = zeesSearchResult.getValue({ name: 'companyname', summmary: search.Operator.GROUP });
+                inlineQty += '<option value="' + zee_id + '">' + zee_name + '</option>';
+                return true;
+            });
+            inlineQty += '</select>';
+            inlineQty += '</div>';
+            inlineQty += '</div>';
+
+            return inlineQty;
+        }
+
+        function parentDropdownSection(inv_type, zee_id) {
+            var inlineQty = '<div class="col-lg-12 custDropdownSection">';
+            inlineQty += '<div class="col-xs-6">';
+            inlineQty += '<div class="input-group"><span class="input-group-addon" id="showMAAP_box">Parent</span></div>';
+            inlineQty += '<select id="zee_dropdown" class="form-control">';
+
+            var customerSearch = search.load({
+                id: ''
+                type: string
+            })
+
+            inlineQty += '<option value="1">Branch</option>';
+            inlineQty += '</select>';
+            inlineQty += '</div>';
+            inlineQty += '</div>';
+
+            return inlineQty;
+        }
+
+        function custDropdownSection(inv_type, zee_id, parent_id) {
+            var inlineQty = '<div class="col-lg-12 subCustDropdownSection">';
+            inlineQty += '<div class="col-xs-6">';
+            inlineQty += '<div class="input-group"><span class="input-group-addon" id="showMAAP_box">Customer</span></div>';
+            inlineQty += '<select id="zee_dropdown" class="form-control">';
+
+            // var search = search.load({
+            //     id: string,
+            //     type: string
+            // });
+
+            inlineQty += '<option value="1">Branch</option>';
+            inlineQty += '</select>';
+            inlineQty += '</div>';
+            inlineQty += '</div>';
+
+            return inlineQty;
+        }
+
+        function subCustDropdownSection(inv_type, zee_id, parent_id, cust_id) {
+            var inlineQty = '<div class="col-lg-12 subCustDropdownSection">';
+            inlineQty += '<div class="col-xs-6">';
+            inlineQty += '<div class="input-group"><span class="input-group-addon" id="showMAAP_box">Sub-Customer</span></div>';
+            inlineQty += '<select id="zee_dropdown" class="form-control">';
+
+            // var search = search.load({
+            //     id: string,
+            //     type: string
+            // });
+
+            inlineQty += '<option value="1">Branch</option>';
+            inlineQty += '</select>';
+            inlineQty += '</div>';
+            inlineQty += '</div>';
+
+            return inlineQty;
+        }
+
+        function generateInvoice(inv_type, zee_id, parent_id, cust_id) {
+
+            // var consol_record = search.load({
+            //     id: 'customsearch_consol_inv_record',
+            //     type: 'customrecord_consol_inv_data'
+            // });
+            // consol_record.filters.push(search.createFilter({
+            //     name: 'custrecord_consol_inv_zee',
+            //     operator: search.Operator.IS,
+            //     values: zee
+            // }));
+
+            var inlineQty = '<div class="col-lg-12 generateInvoiceSection" style="text-align:center">';
+            inlineQty += '<div class="col-xs-6" >';
+            inlineQty += '<button type="button" id="generateInvoice" class="btn btn-primary">Generate Invoice</button>';
+            inlineQty += '</div>';
+            inlineQty += '</div>';
+            inlineQty += '';
+
+            return inlineQty;
+        }
+
+        // function Create_Pdf_files(recType, recordInternalId) { try { 
+        //     nlapiLogExecution('debug', "Printing " + recType + " Internal ID " + recordInternalId); 
+        //     var transNumber = nlapiLookupField('transaction', recordInternalId, 'transactionnumber'); 
+        //     var fileName = transNumber + '.PDF'; 
+        //     var Pdf_Object = nlapiPrintRecord('TRANSACTION', recordInternalId, 'PDF'); 
+        //     Pdf_Object.setFolder(XXX); 
+        //     Pdf_Object.setName(fileName); 
+        //         nlapiSubmitFile(Pdf_Object); /nlapiSendEmail(XXX,XXX,'This Record Has Been Printed','Test',your_email_address@gmail.com',null)/ 
+        //     } catch (err) { 
+        //         nlapiLogExecution('debug', "Error Printing " + recType + " Internal ID " + recordInternalId, err); 
+        //     } 
+        // }
+
+        function isNullorEmpty(strVal) {
+            return (strVal == null || strVal == '' || strVal == 'null' || strVal == undefined || strVal == 'undefined' || strVal == '- None -');
+        }
+
+        return {
+            onRequest: onRequest
+        };
+
+    });
